9504a69cd7a2659f9de7ba71c9ce22687c7e3f07
Samandar <samandarnayimjonov@gmail.com>
2025-04-14 13:29:22 +0500

    done

--- products/views.py
+++ products/views.py
@@ -1,5 +1,4 @@
 from rest_framework.views import APIView
-from rest_framework.response import Response
 from .serializers import ProductSerializer
 from .models import Product
 from .pagination import ProductListPagination
