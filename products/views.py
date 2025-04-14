5c1af30c9fd6d3ff46e06568c97b515a103a59e4
Samandar <samandarnayimjonov@gmail.com>
2025-04-14 12:11:03 +0500

    pagination qushildi

--- products/views.py
+++ products/views.py
@@ -2,11 +2,14 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serializers import ProductSerializer
 from .models import Product
+from .pagination import ProductListPagination
 
 
 class ProductListView(APIView):
     def get(self, request):
-        products = Product.objects.all()
-        serializer = ProductSerializer(products, many=True)
-        return Response(serializer.data)
+        products = Product.objects.select_related('category').all()
+        paginator = ProductListPagination()
+        result_page = paginator.paginate_queryset(products, request)
+        serializer = ProductSerializer(result_page, many=True)
 
+        return paginator.get_paginated_response(serializer.data)
