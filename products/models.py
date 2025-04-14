c8c22e3eac26e64f109365b9626d35a9a14a203c
Samandar <samandarnayimjonov@gmail.com>
2025-04-14 12:15:22 +0500

    update view

--- products/models.py
+++ products/models.py
@@ -3,7 +3,7 @@ from categories.models import Category
 
 
 class Product(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200, db_index=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
     content = models.TextField()
     price = models.DecimalField(max_digits=12, decimal_places=2)
--- products/views.py
+++ products/views.py
@@ -7,9 +7,9 @@ from .pagination import ProductListPagination
 
 class ProductListView(APIView):
     def get(self, request):
-        products = Product.objects.select_related('category').all()
+        products = Product.objects.filter(is_active=True)
+        products = products.select_related('category').all()
         paginator = ProductListPagination()
         result_page = paginator.paginate_queryset(products, request)
         serializer = ProductSerializer(result_page, many=True)
-
         return paginator.get_paginated_response(serializer.data)
