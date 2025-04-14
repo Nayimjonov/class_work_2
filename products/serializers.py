45a164ddbf4444a2aa5e7908eeda1fca592b2570
Samandar <samandarnayimjonov@gmail.com>
2025-04-14 11:56:43 +0500

    products serialzie done

--- products/serializers.py
+++ products/serializers.py
@@ -1,1 +1,9 @@
 from rest_framework import serializers
+
+
+class ProductSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    name = serializers.CharField(read_only=True)
+    category = serializers.CharField(read_only=True, source='category.name')
+    content = serializers.CharField(read_only=True)
+    price = serializers.DecimalField(max_digits=12, decimal_places=2, read_only=True)
